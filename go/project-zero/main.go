d7fff38a2d097ebbf0e0aa0215ea29bc811eb237
Jed Deaver <jedster@mac.com>
2021-06-01 20:54:13 -0400

    added command line parameter and preliminary YAML processing

--- go/project-zero/main.go
+++ go/project-zero/main.go
@@ -1,7 +1,40 @@
 package main
 
-import("fmt")
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Rule struct {
+	Name string `yaml:"name"`
+	Src  string `yaml:"src"`
+	Dst  string `yaml:"dst"`
+}
+
+type Config struct {
+	FirewallNetworkRules []Rule `yaml:"firewall_network_rules"`
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	filePtr := flag.String("file", "creds.yml", "file name containing credentials")
+
+	flag.Parse()
+	fmt.Println("file: ", *filePtr)
+	filename, _ := filepath.Abs(*filePtr)
+	yamlFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	var config Config
+
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Value: %v\n", config.FirewallNetworkRules)
 }
